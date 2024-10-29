877066ea36c5173e59a60e35e102056ffddb448d
Oliver Welsh <104843826+ogb-Welsh@users.noreply.github.com>
2024-10-29 11:41:40 +0200

    push

---
+++ src/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from yoloV8 import *
+
+
+if __name__ == "__main__":
+    video_path = "src/videos/ice_hockey_processed.mp4"
+    model_path = "yolov8s.pt"
+    run_yolo_inference(video_path, model_path)
---
+++ src/yoloV8.py
@@ -0,0 +1,34 @@
+from ultralytics import YOLO
+
+
+def run_yolo_inference(video_path, model_path="yolov8s.pt"):
+
+    model = YOLO(model_path)
+
+    results = model.predict(source=video_path, save=False, device="cuda")
+
+    frames = []
+    bounding_boxes = []
+
+    for result in results:
+        frame = result.orig_img
+        boxes = result.boxes
+
+        frame_boxes = []
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            confidence = box.conf[0]
+            class_id = int(box.cls[0])
+
+            frame_boxes.append(
+                {
+                    "class_id": class_id,
+                    "confidence": confidence,
+                    "box": (x1, y1, x2, y2),
+                }
+            )
+
+        frames.append(frame)
+        bounding_boxes.append(frame_boxes)
+
+    return frames, bounding_boxes
